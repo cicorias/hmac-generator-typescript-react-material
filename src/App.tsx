22995ed8bb23aa69529e6996a94aeafa23d3e10c
Shawn Cicoria <github@cicoria.com>
2021-07-28 22:02:42 -0400

    cleanup css

--- src/App.tsx
+++ src/App.tsx
@@ -131,7 +131,7 @@ function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
+            <Link color="inherit" href="https://www.cicoria.com/">
                 Shawn Cicoria
             </Link>{' '}
             {new Date().getFullYear()}
